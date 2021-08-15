01b182a16b7424d672bb790e1a6ac0e984b6f30c
WanluChung <wanluchung@gmail.com>
2021-08-15 18:08:21 +0800

    add hero-section animation

--- main.js
+++ main.js
@@ -1,23 +1,84 @@
-gsap.registerPlugin(scrollTrigger)
+gsap.registerPlugin(ScrollTrigger)
 
-const splitTextElements = document.querySelectorAll('#key-visual-section .split-version > span')
 
-gsap.scrollTrigger(Array.from(splitTextElements),{
-    y:(index)=>{
-        switch(index){
-           case 0:
-               return '-=15vh';
+gsap.fromTo(document.querySelectorAll('#key-visual-section .left-title'), {
+    x: 0,
+    y: 0,
+}, {
+    x: '+=20vw',
+    y: '-=60px',
+    scrollTrigger: {
+        start: 'top top',
+        end: '+=200vh',
+        scrub: 1,
+    },
+});
+
+gsap.fromTo(document.querySelector('#key-visual-section .right-title'), {
+    x: 0,
+    y: 0,
+}, {
+    x: '-=20vw',
+    y: '-=60px',
+    scrollTrigger: {
+        start: 'top top',
+        end: '+=200vh',
+        scrub: 1,
+    },
+});
+
+gsap.fromTo(document.querySelector('#key-visual-section .first-line'), {
+    x: 0,
+    y: 0,
+    rotateX: 0
+}, {
+    x: 0,
+    y: '-=60px',
+    rotateX: 0,
+    scrollTrigger: {
+        start: 'top top',
+        end: '+=200vh',
+        scrub: 1,
+
+    }
+});
+
+gsap.fromTo(document.querySelector('#key-visual-section .second-line'), {
+    x: 0,
+    y: 0,
+    rotateX: 0
+}, {
+    x: 0,
+    y: '-=40px',
+    rotateX: 0,
+    scrollTrigger: {
+        start: 'top top',
+        end: '+=200vh',
+        scrub: 1,
+
+    }
+});
+
+
+
+const splitTextElements = document.querySelectorAll('#key-visual-section .split-version span')
+
+gsap.to(Array.from(splitTextElements), {
+    y: (index) => {
+        switch (index) {
+            case 0:
+                return '-=15vh';
             case 1:
                 return '-=30vh';
             case 2:
                 return '-=5vh';
             case 3:
-               return '-=23vh';
+                return '-=23vh';
         }
     },
-    scrollTrigger:{
-        start:'top top',
-        end:'bottom bottom',
+    scrollTrigger: {
+        start: 'top top',
+        end: 'bottom bottom',
         scrub: true,
     },
 });
