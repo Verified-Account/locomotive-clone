54c580596357fc5c7ed60048319bd757268bc4a4
WanluChung <wanluchung@gmail.com>
2021-08-16 01:06:48 +0800

    add outline-section list item

--- main.js
+++ main.js
@@ -27,6 +27,19 @@ gsap.fromTo(document.querySelector('#key-visual-section .right-title'), {
     },
 });
 
+gsap.fromTo(document.querySelector('#key-visual-section .logo'), {
+    x: 0,
+    y: 0,
+}, {
+    x: 0,
+    y: '-=80px',
+    scrollTrigger: {
+        start: 'top top',
+        end: '+=200vh',
+        scrub: 1,
+    },
+});
+
 gsap.fromTo(document.querySelector('#key-visual-section .first-line'), {
     x: 0,
     y: 0,
@@ -83,3 +96,48 @@ gsap.to(Array.from(splitTextElements), {
     },
 });
 
+gsap.fromTo(document.querySelector('.divider'), {
+    scaleX: 0,
+}, {
+    scaleX: 1,
+    scrollTrigger: {
+        trigger: '#key-visual-section',
+        start: 'bottom 80%',
+       
+    }
+
+
+});
+
+// gsap.fromTo(document.querySelectorAll('#outline-section .right p'), {
+//     opacity: 0
+// }, {
+//     opacity: 1,
+//     scrollTrigger: {
+//         start: 'top top',
+//         end: 'bottom bottom',
+//         scrub: true,
+       
+//     }
+
+
+// });
+
+gsap.to(document.querySelector('#outline-section .right p'),{
+    scrollTrigger: {
+        trigger: '#key-visual-section',
+        start: 'bottom 50%',
+       
+    },
+    onStart: function() {
+        const elements = document.querySelectorAll('#outline-section .right p');
+
+        Array.from(elements).forEach((element) => {
+            element.classList.remove('initial')
+        })
+        
+    },
+    
+
+
+});
