3a4d322fe67f1d308cd12c4c5d4764d25952da50
WanluChung <wanluchung@gmail.com>
2021-08-15 16:45:52 +0800

    Add Section HTML and hero animation

---
+++ main.js
@@ -0,0 +1,24 @@
+gsap.registerPlugin(scrollTrigger)
+
+const splitTextElements = document.querySelectorAll('#key-visual-section .split-version > span')
+
+gsap.scrollTrigger(Array.from(splitTextElements),{
+    y:(index)=>{
+        switch(index){
+           case 0:
+               return '-=15vh';
+            case 1:
+                return '-=30vh';
+            case 2:
+                return '-=5vh';
+            case 3:
+               return '-=23vh';
+        }
+    },
+    scrollTrigger:{
+        start:'top top',
+        end:'bottom bottom',
+        scrub: true,
+    },
+});
+
